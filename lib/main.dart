f3faf895514d7ec9a1b08aa97d2c6b22a4e9c763
WooRiGyeoRe <rufp0607@naver.com>
2024-07-08 11:15:44 +0900

    커밋 test

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class Home extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Counter(),
+              // Counter(),
               //Buttons(),
             ],
           ),
